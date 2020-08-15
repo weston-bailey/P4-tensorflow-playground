2100aca63d917926dee4a6ef0764899b45e3dc98
Weston Bailey <void@MBP.local>
2020-08-15 16:27:28 -0700

    add tfjs and bootstrap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-console.log('hello front end');
+document.addEventListener('DOMContentLoaded', () => init())
 
-// TODO port tf model to tfjs
+function init() {
+  console.log('hello front end')
+  model = testModel()
+  console.log(model)
+}
+
+function testModel() {
+  //Create the model
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 128, inputShape: [1]})); // layer 1
+  model.add(tf.layers.dense({units: 128, inputShape: [128], activation: 'sigmoid'})); // layer 2
+  model.add(tf.layers.dense({units: 1, inputShape: [128]})); // output layer
+  model.compile({loss: 'meanSquaredError', optimizer: 'adam'}); // compile with params
+
+  return model
+}
