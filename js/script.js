30e65f9920342baa599c40310a73a0402bdd58c3
Weston Bailey <void@MBP.local>
2020-08-15 17:10:44 -0700

    add data script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => init())
 
+console.log('hello front end')
 function init() {
-  console.log('hello front end')
   model = testModel()
   console.log(model)
+  console.log(tf)
+  console.log(tfvis)
+  console.log(MnistData)
+  run()
 }
 
 function testModel() {
@@ -15,6 +19,41 @@ function testModel() {
   model.compile({loss: 'meanSquaredError', optimizer: 'adam'}); // compile with params
 
   return model
+}
+
+async function run() {  
+  const data = new MnistData();
+  await data.load();
+  await showExamples(data);
+}
+
+async function showExamples(data) {
+  // Create a container in the visor
+  const surface =
+    tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});  
+
+  // Get the examples
+  const examples = data.nextTestBatch(20);
+  const numExamples = examples.xs.shape[0];
+  
+  // Create a canvas element to render each example
+  for (let i = 0; i < numExamples; i++) {
+    const imageTensor = tf.tidy(() => {
+      // Reshape the image to 28x28 px
+      return examples.xs
+        .slice([i, 0], [1, examples.xs.shape[1]])
+        .reshape([28, 28, 1]);
+    });
+    
+    const canvas = document.createElement('canvas');
+    canvas.width = 28;
+    canvas.height = 28;
+    canvas.style = 'margin: 4px;';
+    await tf.browser.toPixels(imageTensor, canvas);
+    surface.drawArea.appendChild(canvas);
+
+    imageTensor.dispose();
+  }
 }
 
 // load data
